ad393e0f789e0a68275932c091ec8e5b0809236e
SDJoky <1270849866@qq.com>
2020-02-12 13:24:01 +0800

    init

---
+++ DailyProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DailyProject
+//
+//  Created by Joky_Lee on 2020/2/12.
+//  Copyright © 2020 Joky_Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DailyProject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DailyProject
+//
+//  Created by Joky_Lee on 2020/2/12.
+//  Copyright © 2020 Joky_Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
