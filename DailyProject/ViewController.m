9ff2f037c91a0e998693f839bcb6342c2b4854a4
SDJoky <1270849866@qq.com>
2020-03-04 16:25:08 +0800

    no message

--- DailyProject/ViewController.m
+++ DailyProject/ViewController.m
@@ -36,6 +36,42 @@
     obj1.name = @"name1--";
     [obj1 sd_addObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionNew context:nil];
     obj1.name = @"name-change1---";
+
+    [self xzFuntion];
+}
+
+- (void)mpFuntion {
+//    以内循环交换j j+1数据 第一遍筛选出最小的
+    NSMutableArray *arr = [NSMutableArray arrayWithObjects:@3,@20, @1, @17, @40,nil];
+    NSLog(@"冒泡降序排序前：%@",arr);
+    for (int i = 0; i< arr.count - 1; i++) {
+        for (int j= 0; j< arr.count - 1- i; j ++) {
+            if ([arr[j] intValue] < [arr[j + 1] intValue]) {
+                NSNumber *temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+
+            NSLog(@"mp内循环第%d次排序结果：%@",j+1,arr);
+        }
+    }
+
+}
+
+- (void)xzFuntion {
+    //    第一轮先把最大的排第一位
+    NSMutableArray *arr = [NSMutableArray arrayWithObjects:@3,@20, @1, @17, @40,nil];
+    NSLog(@"选择降序排序前：%@",arr);
+    for (int i = 0; i< arr.count - 1; i++) {
+        for (int j= i+1; j< arr.count; j ++) {
+            if ([arr[i] intValue] < [arr[j] intValue]) {
+                NSNumber *temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            NSLog(@"xz内循环第%d次排序结果：%@",j+1,arr);
+        }
+    }
 }
 
 - (void)viewWillAppear:(BOOL)animated{
